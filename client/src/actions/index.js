4a56ef4b7c8324c305a958fed99d1c7ca8953014
Rahul Arora <rahularorar95@gmail.com>
2019-03-03 22:46:50 +0530

    POST request to add data in db

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,6 @@
 import { SIGN_IN, SIGN_OUT } from "./types"
+import axios from "../apis"
+
 export const signIn = userId => {
   return {
     type: SIGN_IN,
@@ -8,5 +10,11 @@ export const signIn = userId => {
 export const signOut = () => {
   return {
     type: SIGN_OUT
+  }
+}
+
+export const createStream = formValues => {
+  return async dispatch => {
+    axios.post('/streams',formValues)
   }
 }
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 import { Field, reduxForm } from "redux-form"
+import { connect } from "react-redux"
+import { createStream } from "../../actions"
 
 class StreamCreate extends Component {
   renderInput = ({ input, label, meta }) => {
@@ -25,7 +27,7 @@ class StreamCreate extends Component {
   }
 
   onSubmit = formValues => {
-    console.log(formValues)
+    this.props.createStream(formValues)
   }
 
   render() {
@@ -54,7 +56,11 @@ const validate = formValues => {
   return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate)
+
+export default connect(null,{
+  createStream
+})(formWrapped)
