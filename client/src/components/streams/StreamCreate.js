72944194730e2296758372c9c275b7058772327c
Rahul Arora <rahularorar95@gmail.com>
2019-03-03 20:48:29 +0530

    Removed unwanted modules from package.json and implemented error messages in form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,30 +2,58 @@ import React, { Component } from "react"
 import { Field, reduxForm } from "redux-form"
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderInput = ({ input, label, meta }) => {
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {/*similar to <input onChange={input.onChange} value={input.value}/> */}
+        {this.renderError(meta)}
       </div>
     )
   }
 
-  onSubmit(formValues) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      )
+    }
+  }
+
+  onSubmit = formValues => {
     console.log(formValues)
   }
 
   render() {
     return (
-      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
-        <button className="ui button primary">Submit</button>
+        <button type="submit" className="ui button primary">
+          Submit
+        </button>
       </form>
     )
   }
 }
 
+const validate = formValues => {
+  const errors = {}
+  if (!formValues.title) {
+    errors.title = "You must enter a title"
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description"
+  }
+
+  return errors
+}
+
 export default reduxForm({
-  form: "streamCreate"
+  form: "streamCreate",
+  validate
 })(StreamCreate)
