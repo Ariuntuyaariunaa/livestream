0e986aee1a3c8ad79ed9c3e20d49a2ea0d7e36bf
Rahul Arora <rahularorar95@gmail.com>
2019-03-04 01:24:16 +0530

    Adding buttons in stream list based on signed in user

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 import { fetchStreams } from "../../actions"
 export class StreamList extends Component {
@@ -6,10 +7,22 @@ export class StreamList extends Component {
     this.props.fetchStreams()
   }
 
+  renderAdminButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      )
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdminButtons(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -20,18 +33,35 @@ export class StreamList extends Component {
     })
   }
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Available Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) }
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  }
 }
 export default connect(
   mapStateToProps,
