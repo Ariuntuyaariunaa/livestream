e9769245f2af4a9416fa151c74ffaf639d40b7b6
Rahul Arora <rahularorar95@gmail.com>
2019-03-04 17:09:50 +0530

    Making modal component  configurable and reusable

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,8 +1,23 @@
 import React from "react"
 import Modal from "../Modal"
+import history from "../../history"
 
 const StreamDelete = () => {
-  return <Modal />
+  const actions = (
+    <>
+      {/*Similar to <React.Fragment>*/}
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  )
+  return (
+    <Modal
+      title="Delete Stream"
+      content="Are you sure you want to delete this stream ?"
+      actions={actions}
+      onDismiss={() => history.push("/")}
+    />
+  )
 }
 
 export default StreamDelete
