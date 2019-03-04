c7ef817e8fd60758cc6e635bd9e7de5aabc3519d
Rahul Arora <rahularorar95@gmail.com>
2019-03-04 18:23:24 +0530

    Basic setup for rtmp server

---
+++ rtmpserver/index.js
@@ -0,0 +1,20 @@
+//A Node.js implementation of RTMP/HTTP-FLV/WS-FLV/HLS/DASH/MP4 Media Server 
+
+const { NodeMediaServer } = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
